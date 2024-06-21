e78f201d9a6bff7b9ac09dca620969e9c40d6966
Zoran Cvejovic <zcvejovic@gmail.com>
2024-06-21 14:52:09 +0200

    webpack working

---
+++ client/webpack.common.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: {
+    bundle: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "./public/index.html",
+    }),
+    new Dotenv({
+      path:
+        process.env.NODE_ENV === "production"
+          ? "./.env.prod"
+          : process.env.NODE_ENV === "development"
+            ? "./.env.dev"
+            : "./.env.local",
+    }),
+  ],
+};
